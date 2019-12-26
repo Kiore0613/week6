12f73511d7d4ce30bae047858dc9de5a6cc627a4
Kiore <ricardochicas95@gmail.com>
2019-12-25 23:50:24 -0600

    Create project structure (files and folders)

---
+++ homework2/src/app/modules/layout/layout.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormComponent } from './form/form.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PhoneDirective } from './directives/phone/phone.directive';
+import { UrlDirective } from './directives/url/url.directive';
+import { BirthdateDirective } from './directives/birthdate/birthdate.directive';
+
+
+
+@NgModule({
+  declarations: [FormComponent, LayoutComponent, PhoneDirective, UrlDirective, BirthdateDirective],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
